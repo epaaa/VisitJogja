733008578431f91e3765e06d8761ca5e949ae94b
Johevin Blesstowi <BS22-1@slc.net>
2024-02-26 18:50:36 +0700

    navigation hover

--- src/pages/johe/Navigation.tsx
+++ src/pages/johe/Navigation.tsx
@@ -14,15 +14,15 @@ export default function Navigation() {
   }
 
   return (
-    <div className={`absolute top-0 w-screen bg-yellow px-10 py-4 ${responsivePopUp ? 'h-screen' : ''}`}>
+    <div className={`select-none absolute top-0 w-screen bg-yellow px-10 py-4 ${responsivePopUp ? 'h-screen' : ''}`}>
       <div className="flex justify-between items-center">
         <div className="text-2xl font-bold">JOGJAﾟIN</div>
         <div onClick={toggleResponsivePopUp} className="md:hidden block">
           { !responsivePopUp ? <i className="fa-solid fa-bars"></i> : <i className="fa-solid fa-x"></i> }
         </div>
-        <NavigationChild className="md:flex hidden gap-10" />
+        <NavigationChild responsivePopUp={responsivePopUp} className="md:flex hidden gap-10" />
       </div>
-      { !responsivePopUp ? null : <NavigationChild className="mt-10 grid gap-2" /> }
+      { !responsivePopUp ? null : <NavigationChild responsivePopUp={responsivePopUp} className="mt-10 grid gap-2" /> }
     </div>
   )
 }
--- src/pages/johe/NavigationChild.tsx
+++ src/pages/johe/NavigationChild.tsx
@@ -1,14 +1,15 @@
 export interface NavigationChildInterface {
-  className:string
+  className:string,
+  responsivePopUp:boolean
 }
 
 export default function NavigationChild(prop:NavigationChildInterface){
     return (
       <div className={prop.className}>
-        <div className="text-lg font-bold">Home</div>
-        <div className="text-lg font-bold">Places to Go</div>
-        <div className="text-lg font-bold">Things to Do</div>
-        <div className="text-lg font-bold">Budget Calculator</div>
+        <div className="cursor-pointer text-lg font-medium hover:font-bold">Home</div>
+        <div className="cursor-pointer text-lg font-medium hover:font-bold">Places to Go</div>
+        <div className="cursor-pointer text-lg font-medium hover:font-bold">Things to Do</div>
+        <div className="cursor-pointer text-lg font-medium hover:font-bold">Budget Calculator</div>
       </div>
     )
   }
