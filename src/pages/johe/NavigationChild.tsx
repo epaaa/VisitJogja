7f3345b485cc978688cdb7d19e3e33a97059a1f8
Johevin Blesstowi <BS22-1@slc.net>
2024-02-26 18:55:56 +0700

    remove home

--- src/pages/johe/NavigationChild.tsx
+++ src/pages/johe/NavigationChild.tsx
@@ -6,7 +6,6 @@ export interface NavigationChildInterface {
 export default function NavigationChild(prop:NavigationChildInterface){
     return (
       <div className={prop.className}>
-        <div className="cursor-pointer text-lg font-medium hover:font-bold">Home</div>
         <div className="cursor-pointer text-lg font-medium hover:font-bold">Places to Go</div>
         <div className="cursor-pointer text-lg font-medium hover:font-bold">Things to Do</div>
         <div className="cursor-pointer text-lg font-medium hover:font-bold">Budget Calculator</div>
