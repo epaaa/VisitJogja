abc68831cf69105cae08476d2c9610a0e7fc09b9
Johevin Blesstowi <BS22-1@slc.net>
2024-02-26 18:38:16 +0700

    responsive navigation

--- src/pages/johe/Navigation.tsx
+++ src/pages/johe/Navigation.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react"
+import NavigationChild from "./NavigationChild"
+
 export default function Navigation() {
+  const [responsivePopUp, setResponsivePopUp] = useState(false)
+
+  function toggleResponsivePopUp() {
+    setResponsivePopUp(!responsivePopUp)
+    if (!responsivePopUp) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }
+
   return (
-    <div className="bg-yellow flex items-center justify-between px-10 py-8">
-      <div className="text-2xl font-bold">JOGJAﾟIN</div>
-      <div className="flex gap-20">
-        <div className="text-lg font-bold">Home</div>
-        <div className="text-lg font-bold">Places to Go</div>
-        <div className="text-lg font-bold">Things to Do</div>
-        <div className="text-lg font-bold">Budget Calculator</div>
+    <div className={`absolute top-0 w-screen bg-yellow px-10 py-4 ${responsivePopUp ? 'h-screen' : ''}`}>
+      <div className="flex justify-between items-center">
+        <div className="text-2xl font-bold">JOGJAﾟIN</div>
+        <div onClick={toggleResponsivePopUp} className="md:hidden block">
+          { !responsivePopUp ? <i className="fa-solid fa-bars"></i> : <i className="fa-solid fa-x"></i> }
+        </div>
+        <NavigationChild className="md:flex hidden gap-10" />
       </div>
+      { !responsivePopUp ? null : <NavigationChild className="mt-10 grid gap-2" /> }
     </div>
   )
 }
