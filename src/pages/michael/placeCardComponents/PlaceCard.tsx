8b58b6a2da4f5d50ec23f8204349bc165b3a028e
unknown <michaelangelo4489@gmail.com>
2024-02-28 18:14:00 +0700

    things to do card

--- src/pages/michael/placeCardComponents/PlaceCard.tsx
+++ src/pages/michael/placeCardComponents/PlaceCard.tsx
@@ -5,9 +5,11 @@ interface Places {
 
 export default function PlaceCard({ placePic, placeName } : Places) {
   return (
-    <div className="flex flex-col p-3 m-1 rounded-lg">
-      <img src={placePic} className='w-[250px] h-[250px] rounded-lg'></img>
-      <div className="text-center text-brown font-bold m-auto mt-1">{placeName}</div>
-    </div>
+    <a href="">
+      <div className="flex flex-col p-3 m-1 rounded-lg">
+        <img src={placePic} className='w-[250px] h-[250px] rounded-lg '></img>
+        <div className="text-center text-brown font-bold m-auto mt-1">{placeName}</div>
+      </div>
+    </a>
   )
 }
