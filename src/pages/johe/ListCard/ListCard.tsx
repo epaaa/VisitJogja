047a63787eca6180e3c69fc5803fd0555c0f7e37
Johevin Blesstowi <BS22-1@slc.net>
2024-02-28 16:18:15 +0700

    add asset to home

--- src/pages/johe/ListCard/ListCard.tsx
+++ src/pages/johe/ListCard/ListCard.tsx
@@ -4,10 +4,9 @@ export default function ListCard() {
   return (
     <div className="p-10 grid justify-center gap-5">
         <div className="grid gap-5 justify-center">
-            <div className="text-5xl font-semibold text-center">VISIT JOGJA</div>
+          <div className="text-5xl font-semibold text-center px-10">VISIT JOGJA</div>
+          <div className="w-full h-[2px] bg-brown"></div>
         </div>
-
-        <div className="w-full h-[2px] bg-brown"></div>
 
         <div className="grid justify-center grid-cols-3 max-tablet:grid-cols-2 max-mobile:grid-cols-1 items-center gap-9 w-fit">
             <ListCardItem/>        
