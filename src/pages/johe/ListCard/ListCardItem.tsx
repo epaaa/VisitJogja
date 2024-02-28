a6d3b5d4aabae3fe9388031c06e1b624f0041504
Johevin Blesstowi <BS22-1@slc.net>
2024-02-28 17:17:28 +0700

    feature: add like button

--- src/pages/johe/ListCard/ListCardItem.tsx
+++ src/pages/johe/ListCard/ListCardItem.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react"
+
 export default function ListCardItem() {
+  
+  const [isLike, setIsLike] = useState<Boolean>(false)
+
+  function handleLike(){
+    setIsLike(!isLike)
+  }
+
   return (
     <div className="max-w-96 shadow-xl rounded-xl overflow-hidden">
         <img src="../../../../public/list_card/borobudur.png" alt=""/>
@@ -9,10 +18,22 @@ export default function ListCardItem() {
   
             <div className="my-5 w-full h-[2px] bg-brown"></div>
 
-            <div className="flex items-center gap-3 justify-end font-semibold">
-              <div className="">See more</div>
-              <i className="fa-regular fa-circle-right text-2xl"></i>
+            <div className="flex items-center justify-between">
+              <div className="text-xl hover:bg-opacity-40 hover:bg-yellow flex justify-center items-center cursor-pointer w-10 h-10 bg-cream shadow-lg rounded-full" onClick={handleLike}>
+                {
+                  !isLike ? 
+                  <i className="fa-regular fa-heart"></i>
+                  :
+                  <i className="fa-solid fa-heart"></i>
+                }
+              </div>
+
+              <div className="flex items-center gap-3 justify-end font-semibold cursor-pointer hover:bg-opacity-40 hover:bg-yellow p-2 rounded-md">
+                <div className="">See more</div>
+                <i className="fa-regular fa-circle-right text-3xl"></i>
+              </div>
             </div>
+
         </div>
     </div>
   )
