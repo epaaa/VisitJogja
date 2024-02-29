34dfc9345d0fc7b72da06c276722641b8c1fd02d
Evania <evaniaberliantanudirdjo@gmail.com>
2024-02-29 19:19:58 +0700

    popoup card cok

--- src/pages/eva/Eva.tsx
+++ src/pages/eva/Eva.tsx
@@ -1,5 +1,14 @@
-export default function Eva() {
+import Navigation from '../../components/navigation/Navigation';
+import OuterPopUp from '../../components/popUp/OuterPopUp';
+
+function Eva() {
+  
   return (
-    <div>Eva</div>
+    <div className="">
+      <OuterPopUp/>
+      <Navigation/>
+    </div>
   )
 }
+
+export default Eva
