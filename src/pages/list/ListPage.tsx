056cf130ef1cb95f32d6059679fdef160d9c15b2
joheee <94522173+joheee@users.noreply.github.com>
2024-03-03 00:17:01 +0700

    add footer to list page

--- src/pages/list/ListPage.tsx
+++ src/pages/list/ListPage.tsx
@@ -1,15 +1,18 @@
 import Navigation from "../../components/navigation/Navigation";
 import LikeButton from "../../components/LikeButton/LikeButton";
 import ListCard from "../../components/list_card/ListCard";
+import ListPageAbstract from "../../components/ListPageAbstract/ListPageAbstract";
+import FooterList from "../../components/footer/FooterList";
 
 export default function ListPage() {
     
     return (
         <>
         <Navigation/>
-        {/* <ListPageAbstract/> */}
+        <ListPageAbstract/>
         <ListCard/>
         <LikeButton/>
+        <FooterList/>
         </>
     )
 }
