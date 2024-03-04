939c961a987dc05f981d993a1bf75784f2d4a0b8
joheee <94522173+joheee@users.noreply.github.com>
2024-03-05 01:44:35 +0700

    UI budget

--- src/components/BudgetinInput/AutoBudgetin.tsx
+++ src/components/BudgetinInput/AutoBudgetin.tsx
@@ -1,12 +1,11 @@
 export default function AutoBudgetin() {
 
     return (
-        <div className="flex flex-col gap-2 max-w-80">
-            <div className="">Auto Budget'in</div>
-            <div className="">Enter your budget and how many days you want to stay. Voila! All the recommendations for your vacation will be shown</div>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            
+        <div className="flex flex-col gap-5 max-w-96">
+            <div className="text-2xl font-bold text-center">Auto Budget'in</div>
+            <div className="text-center">Enter your budget and how many days you want to stay. Voila! All the recommendations for your vacation will be shown.</div>
+            <input type="number" placeholder="enter your budget" className="bg-white rounded-full border-2 p-3 border-brown hover:bg-brown text-brown hover:text-yellow w-full" />
+            <input type="number" placeholder="nights to stay" className="bg-white rounded-full border-2 p-3 border-brown hover:bg-brown text-brown hover:text-yellow w-full" />
         </div>
     )
 }
--- src/components/BudgetinInput/CustomizeBudgetin.tsx
+++ src/components/BudgetinInput/CustomizeBudgetin.tsx
@@ -1,14 +1,13 @@
 export default function CustomizeBudgetin() {
 
     return (
-        <div className="flex flex-col gap-2 max-w-80">
-            <div className="">Customize</div>
-            <div className="">Enter your budget and divide it for accommodation, food and tours</div>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-
+        <div className="flex flex-col gap-5 max-w-96">
+            <div className="text-2xl font-bold text-center">Customize</div>
+            <div className="text-center">Enter your budget and divide it for accommodation, food and tours.</div>
+            <input type="number" placeholder="nights to stay" className="bg-white rounded-full border-2 p-3 border-brown hover:bg-brown text-brown hover:text-yellow w-full" />
+            <input type="number" placeholder="budget for accommodation" className="bg-white rounded-full border-2 p-3 border-brown hover:bg-brown text-brown hover:text-yellow w-full" />
+            <input type="number" placeholder="budget for food" className="bg-white rounded-full border-2 p-3 border-brown hover:bg-brown text-brown hover:text-yellow w-full" />
+            <input type="number" placeholder="budget for touring" className="bg-white rounded-full border-2 p-3 border-brown hover:bg-brown text-brown hover:text-yellow w-full" />
         </div>
     )
 }
