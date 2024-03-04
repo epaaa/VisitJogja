ffc60b9f5e3a4b617649f37ff637c6d6c4e26afc
joheee <94522173+joheee@users.noreply.github.com>
2024-03-05 03:17:32 +0700

    fix UI in budget page

--- src/components/recommendation/RecommendationCarousel.tsx
+++ src/components/recommendation/RecommendationCarousel.tsx
@@ -4,12 +4,10 @@ import RecommendationCarouselCard from "./RecommendationCarouselCard";
 export default function RecommendationCard({recommendation}: {recommendation: DataInterface[]}) {
  
     return <>
-        <div className="carousel carousel-center max-w-xl p-4 space-x-4">
-        {recommendation.map((item, key) => (
-            <RecommendationCarouselCard key={key} recommendation={item}/>
-        ))}
-
-            
+        <div className="carousel carousel-center max-w-3xl p-4 space-x-4">
+            {recommendation.map((item, key) => (
+                <RecommendationCarouselCard key={key} recommendation={item}/>
+            ))}
         </div>
 
     </>
