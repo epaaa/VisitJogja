f7094699c22213a5ceea580d864b168d4a42458f
unknown <michaelangelo4489@gmail.com>
2024-03-05 19:59:35 +0700

    listpage backdrop

--- src/components/Home/ListPageBackdrop.tsx
+++ src/components/Home/ListPageBackdrop.tsx
@@ -9,25 +9,18 @@ export default function ListPageBackdrop() {
 
   
   return (
-    <div>
-      <div className="relative w-screen">
-        {/* gunung */}
+    <div className="flex flex-col">
+      <div className="relative">
+        {/* frame without shadow  */}
+        <img src={dataContext.imageDic['frame_without_shadow.png']} className="bg-yellow w-full" alt="" />
 
-          <div className="flex-col">
-            <div className="relative w-screen">
-              <div className="relative">
-                <img src={dataContext.imageDic['frame_without_shadow.png']} 
-                  className='bg-yellow w-full shadow-2xl h-[1000px] relative z-50 max-mobile:w-auto max-mobile:h-auto max-tablet:w-auto max-tablet:h-auto'/>
-                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-0 max-mobile:top-28">
-                  <h1 className="font-bold text-brown [font-size:_clamp(16px,5vw,20px)]">{DescriptionDic[navChild!].title}</h1>
-                </div>
-              </div>
-              <img src={dataContext.imageDic['shadow.png']} 
-                className='w-full h-[300px] relative z-50 max-mobile:w-auto max-mobile:h-auto max-tablet:w-auto max-tablet:h-auto'/>
-            </div>
-          </div>
+        {/* title area */}
+        <div className="absolute text-4xl max-mobile:text-3xl font-bold top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">{DescriptionDic[navChild!].title}</div>
 
+        {/* matahari */}
+        <div className="absolute w-[300px] h-[300px] bg-white top-0 left-32 rounded-full"></div>
       </div>
+      <img src={dataContext.imageDic['shadow.png']} alt="" />
     </div>
   )
 }
--- src/components/ListPageAbstract/ListPageAbstractInfromationTemplate.tsx
+++ src/components/ListPageAbstract/ListPageAbstractInfromationTemplate.tsx
@@ -3,7 +3,7 @@ import { AbstractInfromationTemplateInterface } from "../../config/Interface";
 export default function ListPageAbstractInfromationTemplate(prop:AbstractInfromationTemplateInterface) {
   return (
     <div className="max-w-[800px] grid gap-5 p-5">
-        <div className="text-5xl font-semibold text-center">{prop.title}</div>
+        <div className="text-5xl font-semibold text-center max-mobile:text-3xl">{prop.title}</div>
 
         <div className="w-full h-[2px] bg-brown"></div>
 
